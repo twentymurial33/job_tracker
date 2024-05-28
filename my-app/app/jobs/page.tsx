65a42a141321777c7d363c99a646aecf69195264
Murial Anindo <murial.anindo@lonett.com>
2024-05-28 19:26:26 -0400

    added stripe link for testing

--- my-app/app/jobs/page.tsx
+++ my-app/app/jobs/page.tsx
@@ -4,6 +4,12 @@ const JobsPage = () => {
   return (
     <div>
       <h1>jobsPage</h1>
+      <button>    <a
+        href="https://buy.stripe.com/test_5kAbJz0OA2lv2sweUU"
+        className="button"
+      >
+        Click Me
+      </a></button>
     </div>
   )
 }
